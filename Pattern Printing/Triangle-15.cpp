c70e3470ffff5c5d4cba1b6cad5e95777eb3ce21
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-05-19 13:43:19 +0530

    Triangle pattern 15

---
+++ Pattern Printing/Triangle-15.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main (void) {
+    int n; cin >> n;
+
+    for (int i = n; i > 0; i--) {
+        for (int j = n - i; j > 0; j--) {
+            cout << " ";
+        }
+
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
