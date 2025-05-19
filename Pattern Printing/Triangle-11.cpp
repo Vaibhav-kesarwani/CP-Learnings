01186ac8b1f186d16ba4dc73d55d6abec105048f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-05-19 13:21:43 +0530

    Triangle pattern 11

---
+++ Pattern Printing/Triangle-11.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main (void) {
+    int n; cin >> n;   
+    char alpha = 'a';
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << alpha << " ";
+        }
+
+        alpha++;
+        cout << "\n";
+    }
+
+    return 0;
+}
